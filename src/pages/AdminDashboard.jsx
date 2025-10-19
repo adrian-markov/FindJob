530fb9156b99b8b3b8b4349994d97dadb820d5d6
johannacode <johanna.angloma@epitech.com>
2025-10-19 14:58:14 +0200

    Bouton ajout modifié

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "../Components/Navbar.jsx";
-// import Sidebar from "../components/Sidebar";
 import DashboardContent from "../Components/DashboardContent.jsx";
 
 const AdminDashboard = () => {
   return (
     <div className="admin-container">
       <div className="content-container">
-        {/* <Sidebar /> */}
         <DashboardContent />
       </div>
     </div>
